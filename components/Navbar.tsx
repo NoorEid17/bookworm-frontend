dab62bbddf29c8171328fb124669afd2c8525b5e
Noor Eid <noorhamdyeid19@gmail.com>
2023-07-13 10:58:44 +0200

    Init the app

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+const Navbar = () => {
+  return (
+    <nav className="min-h-5 flex justify-between p-2">
+        <div></div>
+        <div>
+            <Image src="/assets/Logo.png" alt="Logo" width={150} height={24} />
+        </div>
+        <div>
+            switch theme
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
