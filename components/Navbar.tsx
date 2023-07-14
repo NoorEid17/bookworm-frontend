b6483d99a543387a00edf87aa4c47e253b486448
Noor Eid <noorhamdyeid19@gmail.com>
2023-07-14 11:10:49 +0200

    Added AuthProvider

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,18 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="min-h-5 flex justify-between p-2">
-        <div></div>
-        <div>
-            <Image src="/assets/Logo.png" alt="Logo" width={150} height={24} />
-        </div>
-        <div>
-            .
-        </div>
+      <div></div>
+      <div>
+        <Link href="/">
+          <Image src="/assets/Logo.png" alt="Logo" width={150} height={24} />
+        </Link>
+      </div>
+      <div>.</div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
