61b60bbdf493f3ec30794b3709b946b73059339b
Noor Eid <noorhamdyeid19@gmail.com>
2023-07-20 09:37:49 +0200

    changed refresh token api to return only the token

--- api/user.ts
+++ api/user.ts
@@ -10,8 +10,10 @@ export const login = (data: UserCredentials): Promise<AxiosResponse> => {
   return axios.post("/user/login", data, { withCredentials: true });
 };
 
-export const refreshToken = (): Promise<AxiosResponse> => {
-  return axios.get("/user/token", { withCredentials: true });
+export const refreshToken = (): Promise<string> => {
+  return axios
+    .get("/user/token", { withCredentials: true })
+    .then((res) => res.data.token);
 };
 
 export const logut = (): Promise<AxiosResponse> => {
@@ -20,4 +22,15 @@ export const logut = (): Promise<AxiosResponse> => {
 
 export const signup = (data: any): Promise<AxiosResponse> => {
   return axios.post("/user/signup", data);
+};
+
+export const update = (data: any) => {
+  const formData = new FormData();
+  for (const [key, value] of Object.entries(data)) {
+    if (!value) {
+      continue;
+    }
+    formData.append(key, value as string | Blob);
+  }
+  return axios.patch("/user/update", formData, { withCredentials: true });
 };
