aa4585048c11c4347b101ecbee14219cff66bd68
Noor Eid <noorhamdyeid19@gmail.com>
2023-09-14 22:58:55 +0300

    added categories list on the right

--- api/category.ts
+++ api/category.ts
@@ -1,9 +1,16 @@
 import axios from "./axios";
 
+export interface Category {
+  name: string;
+  categoryId: string;
+  slug: string;
+}
+
 export const createCategory = async (data: { name: string }) => {
   return axios.post("/category/create", data, { withCredentials: true });
 };
 
-export const fetchAllCategories = async () => {
-  return axios.get("/category/all");
+export const fetchAllCategories = async (): Promise<Category[]> => {
+  const { data } = await axios.get("/category/all");
+  return data.categories;
 };
